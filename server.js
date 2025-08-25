453a835f35de02b31174c37fbb8bb32217db411b
oosmajid <oosmajid@gmail.com>
2025-08-25 21:27:07 +0330

    test

--- server.js
+++ server.js
@@ -482,11 +482,13 @@ app.post('/api/user/:telegram_id/period', async (req, res) => {
         // Notify companions that period has started
         const today = moment().format('YYYY-MM-DD');
         if (start_date === today) {
+            console.log('--HERE--')
             const companionsRes = await client.query('SELECT companion_telegram_id FROM companions WHERE user_id = $1', [userId]);
             companionsRes.rows.forEach(c => {
                 const message = getRandomMessage('companion', 'period_started').replace('{FIRST_NAME}', userFirstName);
                 bot.sendMessage(c.companion_telegram_id, message);
             });
+            bot.sendMessage("HIII");
         }
 
         await client.query('COMMIT');
